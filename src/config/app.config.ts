ef5e973af25bb4dbbf8fcd53bb87e09642bfa29d
Zanaen <zanaen@vrox.co.uk>
2025-01-27 10:42:15 +0500

    app.config.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,20 +1,23 @@
-import { getEnv } from "../utils/get-env";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 const appConfig = () => ({
-  NODE_ENV: getEnv("NODE_ENV", "development"),
-  PORT: getEnv("PORT", "5000"),
-  BASE_PATH: getEnv("BASE_PATH", "/api"),
-  MONGO_URI: getEnv("MONGO_URI", ""),
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || "8000",
+  BASE_PATH: process.env.BASE_PATH || "/api",
+  MONGO_URI: process.env.MONGO_URI || "",
 
-  SESSION_SECRET: getEnv("SESSION_SECRET"),
-  SESSION_EXPIRES_IN: getEnv("SESSION_EXPIRES_IN"),
+  SESSION_SECRET: process.env.SESSION_SECRET || "",
+  SESSION_EXPIRES_IN: process.env.SESSION_EXPIRES_IN || "1d",
 
-  GOOGLE_CLIENT_ID: getEnv("GOOGLE_CLIENT_ID"),
-  GOOGLE_CLIENT_SECRET: getEnv("GOOGLE_CLIENT_SECRET"),
-  GOOGLE_CALLBACK_URL: getEnv("GOOGLE_CALLBACK_URL"),
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL || "",
 
-  FRONTEND_ORIGIN: getEnv("FRONTEND_ORIGIN", "localhost"),
-  FRONTEND_GOOGLE_CALLBACK_URL: getEnv("FRONTEND_GOOGLE_CALLBACK_URL"),
+  FRONTEND_ORIGIN: process.env.FRONTEND_ORIGIN || "http://localhost:3000",
+  FRONTEND_GOOGLE_CALLBACK_URL: process.env.FRONTEND_GOOGLE_CALLBACK_URL || "",
 });
 
 export const config = appConfig();
