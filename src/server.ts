15c01b06dff7aa723c797bd30842d187435d2fdc
Muhammad Zanaen Ullah <zanaenullah75@gmail.com>
2025-10-20 09:49:31 +0500

    test

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import serverless from "serverless-http";
-import app from "./app";
-import connectDatabase from "../src/config/database.config";
-
-// Connect to DB on cold start
-connectDatabase();
-
-export const handler = serverless( app );
